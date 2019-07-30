bad5dc1d7d4d07b866dcf7ab45acb2fac2cb9896
pdw <devdw98@gmail.com>
2019-07-30 22:40:27 +0900

    setting

--- dw/settings.py
+++ dw/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ls-jp(gdxdzd&s@kf0&jjo$ri27z=g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
